7c52731e0dc60a6e959c05202503d674571458f9
Johannes Chorzempa <jchorzempa@posteo.org>
2025-07-08 13:23:36 +0200

    make keydown responsive modal

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,12 @@
+import Navigation from '@/components/Navigation';
 import GlobalStyles from '@/styles/GlobalStyles';
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import {
+	Outlet,
+	createRootRoute,
+	useRouterState,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export const Route = createRootRoute({
@@ -9,22 +14,36 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
-	const [active, setActive] = useState(true);
+	const [active, setActive] = useState(false);
+	const routerState = useRouterState(); //get the pathname to toggle menu only on non root paths
 
-	const toggleMenuOnKeyDown = (event: KeyboardEvent) => {
-		if (event.key === 'Escape') {
-			setActive(!active);
-		}
-	};
+	useEffect(() => {
+		const toggleMenuOnKeyDown = (event: KeyboardEvent) => {
+			if (event.key !== 'Escape') return;
+			if (routerState.location.pathname === '/') return;
 
-	document.addEventListener('keydown', toggleMenuOnKeyDown);
+			setActive((prev) => !prev);
+		};
+
+		document.addEventListener('keydown', toggleMenuOnKeyDown);
+
+		return () => {
+			document.removeEventListener('keydown', toggleMenuOnKeyDown);
+		};
+	}, [routerState.location.pathname]); // reattach if pathname changes
+
+	useEffect(() => {
+		// Close modal when route changes
+		console.log(routerState.location.pathname);
+		setActive(false);
+	}, [routerState.location.pathname]);
 
 	return (
 		<>
 			<GlobalStyles />
 			<Outlet />
 			<Overlay $active={active}>
-				<div style={{ cursor: 'pointer' }}>X</div>
+				<Navigation />
 			</Overlay>
 			<TanStackRouterDevtools />
 		</>
@@ -37,12 +56,12 @@ const Overlay = styled.div<{ $active: boolean }>`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  color: white;
+  background-color: rgb(0, 0, 0);
+
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 999;
+  z-index: ${({ $active }) => ($active ? 100 : -1)};
   opacity: ${({ $active }) => ($active ? 1 : 0)};
   transition: opacity 0.3s ease-in-out;
 `;
