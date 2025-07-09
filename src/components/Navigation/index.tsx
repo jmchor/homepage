821f106433efbbad2c6cbbb449212e8f2a5318ff
Johannes Chorzempa <jchorzempa@posteo.org>
2025-07-09 13:08:01 +0200

    send state to Nav component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,15 +1,23 @@
 import { Link } from '@tanstack/react-router';
 import Footer from '../Footer';
 import {
-	NavigationWrapper,
-	Stars,
-	MoreStars,
 	EvenMoreStars,
+	MoreStars,
 	NavigationInnerWrapper,
 	NavigationList,
+	NavigationWrapper,
+	Stars,
 } from './styles';
 
-export default function Navigation({ location }: { location: string }) {
+export default function Navigation({
+	location,
+	active,
+	setActive,
+}: {
+	location: string;
+	active: boolean;
+	setActive: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
 	return (
 		<NavigationWrapper>
 			<Stars />
@@ -41,7 +49,8 @@ export default function Navigation({ location }: { location: string }) {
 						</li>
 					</NavigationList>
 				</nav>
-				{location !== '/' && (
+
+				{location !== '/' && window.innerWidth > 1200 && (
 					<Footer
 						text="Exit Menu"
 						bgColor="radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%)"
