18c1a79820a396bd1061bb0f0e5a3ece836532d6
Johannes Chorzempa <jchorzempa@posteo.org>
2025-07-10 16:50:55 +0200

    create dynamic for available routes

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,29 +1,71 @@
 import { Link } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
 import Footer from '../Footer';
 import {
+	EvenMoreStars,
+	MoreStars,
 	NavigationInnerWrapper,
 	NavigationList,
 	NavigationWrapper,
+	Stars,
 } from './styles';
 
-export default function Navigation({ location }: { location: string }) {
+type Route = {
+	path: string;
+	label: string;
+};
+
+export default function Navigation({
+	currentLocation,
+}: { currentLocation: string }) {
+	const [filteredRoutes, setFilteredRoutes] = useState<Route[]>([]);
+	const routes: Route[] = [
+		{
+			path: '/about',
+			label: 'about',
+		},
+		{
+			path: '/development',
+			label: 'development',
+		},
+		{
+			path: '/activism',
+			label: 'activism',
+		},
+		{
+			path: '/music',
+			label: 'music',
+		},
+	];
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		// If the current path is '/', show all routes
+		if (currentLocation === '/') {
+			setFilteredRoutes(routes);
+		} else {
+			// Otherwise, filter out the current page's path
+			const updatedRoutes = routes.filter(
+				(route) => route.path !== currentLocation,
+			);
+			setFilteredRoutes(updatedRoutes);
+		}
+	}, [currentLocation]);
+
 	return (
 		<NavigationWrapper>
+			<Stars />
+			<MoreStars />
+			<EvenMoreStars />
 			<NavigationInnerWrapper>
 				<nav>
 					<NavigationList>
-						<li>
-							<Link to="/about">about</Link>
-						</li>
-						<li>
-							<Link to="/development">development</Link>
-						</li>
-						<li>
-							<Link to="/activism">activism</Link>
-						</li>
-						<li>
-							<Link to="/music">music</Link>
-						</li>
+						{filteredRoutes.length > 0 &&
+							filteredRoutes.map((route) => (
+								<li key={route.path}>
+									<Link to={route.path}>{route.label}</Link>
+								</li>
+							))}
 						<li>
 							<a
 								href="https://papertrail.jmchor.dev/"
@@ -36,7 +78,7 @@ export default function Navigation({ location }: { location: string }) {
 					</NavigationList>
 				</nav>
 
-				{location !== '/' && window.innerWidth > 1200 && (
+				{currentLocation !== '/' && window.innerWidth > 1200 && (
 					<Footer active={true} bgColor="var(--endlessSpace)" />
 				)}
 			</NavigationInnerWrapper>
