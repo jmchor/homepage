2cc3ce4cbe48508b63694199e59729429a16b823
Johannes Chorzempa <jchorzempa@posteo.org>
2025-07-10 19:53:55 +0200

    move data to external file

---
+++ src/data/DevProjects.ts
@@ -0,0 +1,147 @@
+import type { Project } from '@/types/projects';
+
+export const DevProjects: Project[] = [
+	{
+		title: 'Project 1',
+		description: 'Description 1',
+		link: 'https://example.com/project1',
+		thumbnail: {
+			src: '/favicon.ico',
+			alt: 'Thumbnail 1',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 2',
+		description: 'Description 2',
+		link: 'https://example.com/project2',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 2',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 3',
+		description: 'Description 3',
+		link: 'https://example.com/project3',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 3',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 4',
+		description: 'Description 4',
+		link: 'https://example.com/project4',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 4',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+	{
+		title: 'Project 5',
+		description: 'Description 5',
+		link: 'https://example.com/project5',
+		thumbnail: {
+			src: 'favicon.ico',
+			alt: 'Thumbnail 5',
+			width: 150,
+			height: 150,
+		},
+	},
+];
