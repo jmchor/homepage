7bb663f9111896c4b067d1eda65d605bea0345ca
Johannes Chorzempa <jchorzempa@posteo.org>
2025-07-10 19:54:22 +0200

    create new Style

---
+++ src/styles/GlobalWebpageStyle.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const GlobalWebpageStyle = styled.div`
+width: 100%;
+min-height: 100vh;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding: 50px;
+
+`;
